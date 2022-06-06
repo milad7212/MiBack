5f829ce0c00c3685c5ff4bbb9a23d2e52022324d
miillaadddddd <milad7212@gmail.com>
2022-06-06 18:10:40 +0430

    TWO

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Joi = require("joi");
+
 const express = require("express");
 const app = express();
 
@@ -17,11 +19,16 @@ app.get("/", (req, res) => {
     res.send("Hello MILAD");
 });
 
-app.get("/api/courses", (req, res) => {
-    res.send(courses);
-});
+app.post("/api/courses", (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+    const result = schema.validate({});
+    if (result.error) {
+        res.status(400).send(result.error.details);
+        return;
+    }
 
-app.post("api/corses", (req, res) => {
     const course = {
         id: courses.length + 1,
         name: req.body.name,
@@ -29,26 +36,6 @@ app.post("api/corses", (req, res) => {
     courses.push(course);
     res.send(course);
 });
-
-app.get("/api/courses/:id", (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course)
-        return res.status(404).send(`dadash nist ${req.params.id} moredd`);
-
-    res.send(course);
-});
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course)
-        return res.status(404).send(`dadash nist ${req.params.id} moredd`);
-
-    // Validate
-
-    // If invalid, return 400 - Bad request
-
-    // Update Course 
-    // Return the updated course
-})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
